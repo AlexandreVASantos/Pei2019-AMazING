d17de7717c62e9d7fde9260efd101197363cbc0c
miguewinho <mig.reis@ua.pt>
2019-03-13 12:59:36 +0000

    Alterações na WebApp

--- venv/AMazING/main/views.py
+++ venv/AMazING/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	return render(request,'main/home.html')
+	return render(request,'main/login.html')
 
 def about(request):
 	return HttpResponse('<h1> about </h1>')
