49e1187d9f983651325bce62081b5a34a098ebae
Alexandre <alexandresantos1@ua.pt>
2019-03-28 19:42:39 +0000

    switch controller buttons green for ON and red for OFF

--- SwitchController/SwitchController/Controller/views.py
+++ SwitchController/SwitchController/Controller/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+
 import json
 import requests
 
-grid={'1':'OFF','2':'OFF','3':'OFF','4':'OFF','5':'OFF','6':'OFF','7':'OFF','8':'OFF','9':'OFF','10':'OFF','11':'OFF','12':'OFF','13':'OFF','14':'OFF','15':'OFF','16':'OFF','17':'OFF','18':'OFF','19':'OFF','20':'OFF','21':'OFF','22':'OFF','23':'OFF','24':'OFF'}
+grid={'1':('OFF','red'),'2':('OFF','red'),'3':('OFF','red'),'4':('OFF','red'),'5':('OFF','red'),'6':('OFF','red'),'7':('OFF','red'),'8':('OFF','red'),'9':('OFF','red'),'10':('OFF','red'),'11':('OFF','red'),'12':('OFF','red'),'13':('OFF','red'),'14':('OFF','red'),'15':('OFF','red'),'16':('OFF','red'),'17':('OFF','red'),'18':('OFF','red'),'19':('OFF','red'),'20':('OFF','red'),'21':('OFF','red'),'22':('OFF','red'),'23':('OFF','red'),'24':('OFF','red')}
 # Create your views here.
 def home(request):
 	return render(request,'Controller/home.html')
+
 def send_grid(request):
-	return render(request,'Controller/config.html',grid)
+	node_grid = grid
+	return render(request,'Controller/config.html',{'node_grid':node_grid})
 
 def send_shit(request):
 	
@@ -26,21 +28,21 @@ def send_shit(request):
 		if value == 'OFF':
 			print('dnfkjsfjksdjfsnfdgkdfgkdkjfsdkjfbsdk')
 		#turn on
-			grid[node] ="ON"
+			grid[node] =('ON',"green")
 		else:
 		#turn off
 			print("blablablabla")
-			grid[node] ="OFF"
+			grid[node] =('OFF',"red")
 		#
 		print(grid['1'])
 		print(grid)
 		print(value)
 		print(node)
 		
-
+		node_grid = grid
 		data_json= json.dumps(data)
 		headers = {'Content-Type': 'application/json'}
 		req= requests.post(url,data=data_json, headers=headers)
 		print(req.json())
 		
-		return render(request, 'Controller/config.html', grid)
+		return render(request, 'Controller/config.html',{'node_grid':node_grid})
