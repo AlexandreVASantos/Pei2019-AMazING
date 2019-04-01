d4956797e93c5f749da0e50a302b5cf7467368a9
Alexandre <alexandresantos1@ua.pt>
2019-04-01 20:29:57 +0100

    added database to support node grid

--- SwitchController/SwitchController/Controller/views.py
+++ SwitchController/SwitchController/Controller/views.py
@@ -2,47 +2,70 @@ from django.shortcuts import render
 
 import json
 import requests
+import os
+import subprocess
+import sqlite3
 
-grid={'1':('OFF','red'),'2':('OFF','red'),'3':('OFF','red'),'4':('OFF','red'),'5':('OFF','red'),'6':('OFF','red'),'7':('OFF','red'),'8':('OFF','red'),'9':('OFF','red'),'10':('OFF','red'),'11':('OFF','red'),'12':('OFF','red'),'13':('OFF','red'),'14':('OFF','red'),'15':('OFF','red'),'16':('OFF','red'),'17':('OFF','red'),'18':('OFF','red'),'19':('OFF','red'),'20':('OFF','red'),'21':('OFF','red'),'22':('OFF','red'),'23':('OFF','red'),'24':('OFF','red')}
+
+
+
+grid = {}
+
 # Create your views here.
 def home(request):
 	return render(request,'Controller/home.html')
 
 def send_grid(request):
+	refresh_grid()
 	node_grid = grid
 	return render(request,'Controller/config.html',{'node_grid':node_grid})
 
-def send_shit(request):
+def refresh_grid():
+	connection = sqlite3.connect("/home/alexandre/Desktop/SwitchController/SwitchController/Controller/grid.db")
+	c=connection.cursor()
+	c.execute("Select * from node;")
+	fetch= c.fetchall()
+	for row in fetch:
+		grid[row[0]] = (row[1],row[2],row[3])
+	connection.close()
 	
-		#url = 'http://192.1.1.1/connection'
-		url = "http://httpbin.org/post"
-		#data = {"SSID":"foo","Frequency":"1234","WEP":"1234123asdgwer"}
-		data = {"data":"123"}
-		dic=request.GET
-		for key in dic:
-			node=key
-			value=dic[key]
-		
-		
-		print(value)
-		if value == 'OFF':
-			print('dnfkjsfjksdjfsnfdgkdfgkdkjfsdkjfbsdk')
-		#turn on
-			grid[node] =('ON',"green")
-		else:
-		#turn off
-			print("blablablabla")
-			grid[node] =('OFF',"red")
-		#
-		print(grid['1'])
-		print(grid)
-		print(value)
-		print(node)
-		
-		node_grid = grid
-		data_json= json.dumps(data)
-		headers = {'Content-Type': 'application/json'}
-		req= requests.post(url,data=data_json, headers=headers)
-		print(req.json())
-		
-		return render(request, 'Controller/config.html',{'node_grid':node_grid})
+
+
+def send_shit(request):
+	#os.system('ping google.com')
+	subprocess.check_call(['echo','SWITCH ARUBA'])
+	#url = 'http://192.1.1.1/connection'
+	url = "http://httpbin.org/post"
+	#data = {"SSID":"foo","Frequency":"1234","WEP":"1234123asdgwer"}
+	data = {"data":"123"}
+	dic=request.GET
+	for key in dic:
+		node=key
+		value=dic[key]
+
+	comands = 'telnet x.x.x.x configure terminal interface' 
+	
+	connection = sqlite3.connect("/home/alexandre/Desktop/SwitchController/SwitchController/Controller/grid.db")
+	c=connection.cursor()
+	
+	if value == 'OFF':
+	#turn on
+		query = "Update node Set value='ON', color='green' where id="+ node +";"
+	else:
+	#turn off
+		query = "Update node Set value='OFF', color='red' where id=" + node +";"
+	
+	c.execute(query)
+	connection.commit()
+	connection.close()
+	#
+	
+	refresh_grid()
+	print(grid)
+	node_grid = grid
+	data_json= json.dumps(data)
+	headers = {'Content-Type': 'application/json'}
+	req= requests.post(url,data=data_json, headers=headers)
+	print(req.json())
+	
+	return render(request, 'Controller/config.html',{'node_grid':node_grid})
