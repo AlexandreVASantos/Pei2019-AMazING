ab43e5c56bba030b6d4f528181263312da56597d
Alexandre <alexandresantos1@ua.pt>
2019-04-25 12:21:12 +0100

    final version

--- install_node.sh
+++ install_node.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo mkdir /home/amazing/dont_delete
 
 echo 'amazing' | sudo -S mkdir /home/amazing/dont_delete
 
@@ -12,11 +11,11 @@ echo 'amazing' | sudo -S chmod 775 /home/amazing/dont_delete/init_node.sh
 
 echo 'amazing' | sudo -S apt-get install python-pip -y
 
-echo 'amazing' | sudo -S pip install virtualenv -y
+echo 'amazing' | sudo -S pip install virtualenv
 
 virtualenv /home/amazing/dont_delete/env
 
 source /home/amazing/dont_delete/env/bin/activate
 
-echo 'amazing' | sudo -S pip install -r /home/amazing/dont_delete_requirements.txt
+echo 'amazing' | sudo -S pip install -r /home/amazing/dont_delete/requirements.txt
 
