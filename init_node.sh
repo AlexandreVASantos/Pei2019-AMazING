5344f59f00aa9148a4a04fdbf4a170a79e15e3ea
Alexandre <alexandresantos1@ua.pt>
2019-04-25 11:50:08 +0100

    alteration on scripts

---
+++ init_node.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+source /home/amazing/dont_delete/env/bin/activate
+
+
+python3 /home/amazing/dont_delete/APIPEI.py
--- install_node.sh
+++ install_node.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-
-
-
-#echo 'amazing' | sudo -S mount /dev/sdb1 /media/cdrom 
+sudo mkdir /home/amazing/dont_delete
 
 echo 'amazing' | sudo -S mkdir /home/amazing/dont_delete
 
@@ -11,7 +8,7 @@ echo 'amazing' | sudo -S cp /media/cdrom/* /home/amazing/dont_delete/
 
 echo 'amazing' | sudo -S chown root /home/amazing/dont_delete/init_node_script.sh
 
-echo 'amazing' | sudo -S chmod 775 /home/amazing/dont_delete/init_node_script.sh
+echo 'amazing' | sudo -S chmod 775 /home/amazing/dont_delete/init_node_sript.sh
 
 echo 'amazing' | sudo -S apt-get install python-pip -y
 
@@ -19,8 +16,7 @@ echo 'amazing' | sudo -S pip install virtualenv -y
 
 virtualenv /home/amazing/dont_delete/env
 
-source /home/amazing/dont_delete/venv/bin/activate
+source /home/amazing/dont_delete/env/bin/activate
 
-pip install -r /home/amazing/dont_delete/requirements.txt
-
+echo 'amazing' | sudo -S pip install -r /home/amazing/dont_delete_requirements.txt
 
