e85ab8094f453c7aafb209b19b80f042542fe3ad
luissantana <luis.santana@ua.pt>
2019-05-07 19:24:09 +0100

    added verifying function and connected them to post functions

--- venv/AMazING/main/views.py
+++ venv/AMazING/main/views.py
@@ -1,6 +1,8 @@
 import requests
+import re
 import json
 from django.shortcuts import render
+from django.contrib import messages
 from django.http import HttpResponse
 from kafka import KafkaProducer
 import datetime
@@ -58,124 +60,188 @@ def postScanning(request):
 	url = "http://httpbin.org/post"
 	#url = "http://192.168.0.25:5000/changeIP"
 	data = request.GET
-	
-	#dic = {"data":"123"}
 	dic={}
-	for key in data:
-		dic[key] = data[key]
+	#print(data)
 
+	if(verify_Wlan(request,data)):	
 		
+		for key in data:
+			print(key)
+			dic[key] = data[key]
+
+		dataToSend = json.dumps(dic)
+		headers = {'Content-Type': 'application/json'}
+		req = requests.post(url,data=dataToSend, headers=headers)
+		print(req.json())
+		return render(request, 'main/menu.html', {"flagEL":False,"flagSC":True,"flagLS":False,"flagIPC":False,"flagSS":False,"flagSP":False,"flagTP":False,"flagC":False})
+
 	dataToSend = json.dumps(dic)
 	headers = {'Content-Type': 'application/json'}
 	req = requests.post(url,data=dataToSend, headers=headers)
 	print(req.json())
-	return render(request, 'main/menu.html', {"flagEL":False,"flagSC":True,"flagLS":False,"flagIPC":False,"flagSS":False,"flagSP":False,"flagTP":False,"flagC":False})
+	return render(request,'main/menu.html')	
 
 def postLinkStatus(request):
 	url = "http://httpbin.org/post"
 	#url = "http://192.168.0.25:5000/changeIP"
 	data = request.GET
-	
-	#dic = {"data":"123"}
 	dic={}
-	for key in data:
-		dic[key] = data[key]
+	#print(data)
 
+	if(verify_Wlan(request,data)):	
 		
+		for key in data:
+			print(key)
+			dic[key] = data[key]
+
+		dataToSend = json.dumps(dic)
+		headers = {'Content-Type': 'application/json'}
+		req = requests.post(url,data=dataToSend, headers=headers)
+		print(req.json())
+		return render(request, 'main/menu.html', {"flagEL":False,"flagSC":False,"flagLS":True,"flagIPC":False,"flagSS":False,"flagSP":False,"flagTP":False,"flagC":False})
 	dataToSend = json.dumps(dic)
 	headers = {'Content-Type': 'application/json'}
 	req = requests.post(url,data=dataToSend, headers=headers)
 	print(req.json())
-	return render(request, 'main/menu.html', {"flagEL":False,"flagSC":False,"flagLS":True,"flagIPC":False,"flagSS":False,"flagSP":False,"flagTP":False,"flagC":False})
+	return render(request,'main/menu.html')
+
 
 
 def postIPChange(request):
 	url = "http://httpbin.org/post"
 	#url = "http://192.168.0.25:5000/changeIP"
 	data = request.GET
-	
-	#dic = {"data":"123"}
 	dic={}
-	for key in data:
-		dic[key] = data[key]
-
+	
+	print(verify_IP(request,data["IP"]))
+	if(verify_IP(request,data["IP"]) and not (len(data)==0)):	
 		
+		for key in data:
+			print(key)
+			dic[key] = data[key]
+
+		dataToSend = json.dumps(dic)
+		headers = {'Content-Type': 'application/json'}
+		req = requests.post(url,data=dataToSend, headers=headers)
+		print(req.json())
+		return render(request, 'main/menu.html', {"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":True,"flagSS":False,"flagSP":False,"flagTP":False,"flagC":False})
+	
 	dataToSend = json.dumps(dic)
 	headers = {'Content-Type': 'application/json'}
 	req = requests.post(url,data=dataToSend, headers=headers)
 	print(req.json())
-	return render(request, 'main/menu.html', {"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":True,"flagSS":False,"flagSP":False,"flagTP":False,"flagC":False})
+	return render(request,'main/addrChange.html')
 
 def postConnection(request):
 	url = "http://httpbin.org/post"
 	#url = "http://192.168.0.25:5000/connect"
 	data = request.GET
+	dic={}
 	#print(data)
 	#dic = {"data":"123"}
 	#dic = {"data":"123"}
-	dic={}
-	for key in data:
-		dic[key] = data[key]
+	
+	if(verify_Wlan(request,data) and not (len(data)==0)):	
+		print(verify_Wlan(request,data))
+		for key in data:
+			print(key)
+			dic[key] = data[key]
 
-		
+		dataToSend = json.dumps(dic)
+		headers = {'Content-Type': 'application/json'}
+		req = requests.post(url,data=dataToSend, headers=headers)
+		print(req.json())
+		return render(request, 'main/connection.html',{"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":False,"flagSS":False,"flagSP":False,"flagTP":False,"flagC":True})
+			
 	dataToSend = json.dumps(dic)
 	headers = {'Content-Type': 'application/json'}
 	req = requests.post(url,data=dataToSend, headers=headers)
 	print(req.json())
-	return render(request, 'main/connection.html',{"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":False,"flagSS":False,"flagSP":False,"flagTP":False,"flagC":True})
+	return render(request,'main/connection.html')
+	
 
 def postStationStats(request):
 	url = "http://httpbin.org/post"
 	#url = "http://192.168.0.25:5000/changeIP"
-	data = request.GET 										#########NOT WORKING request.GET vazio 
-					
-	#dic = {"data":"123"}
-	#dic = {"data":"123"}
+	data = request.GET
 	dic={}
-	for key in data:
-		dic[key] = data[key]
+	#print(data)
 
+	if(verify_Wlan(request,data)):	
 		
+		for key in data:
+			print(key)
+			dic[key] = data[key]
+
+		dataToSend = json.dumps(dic)
+		headers = {'Content-Type': 'application/json'}
+		req = requests.post(url,data=dataToSend, headers=headers)
+		print(req.json())
+		return render(request, 'main/menu.html', {"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":False,"flagSS":True,"flagSP":False,"flagTP":False,"flagC":False})
 	dataToSend = json.dumps(dic)
 	headers = {'Content-Type': 'application/json'}
 	req = requests.post(url,data=dataToSend, headers=headers)
 	print(req.json())
-	return render(request, 'main/menu.html', {"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":False,"flagSS":True,"flagSP":False,"flagTP":False,"flagC":False})
+	return render(request,'main/menu.html')
+
 
 def postStationPeer(request):
 	url = "http://httpbin.org/post"
 	#url = "http://192.168.0.25:5000/changeIP"
 	data = request.GET
-	print(data)
+	#print(data)
 	#dic = {"data":"123"}
-
+	mac=data["MAC"]
 	dic={}
-	for key in data:
-		dic[key] = data[key]
 
-		
+	if not (len(data)==0):
+		if(verify_Wlan(request,data) and verify_mac(request,mac)):
+				
+			
+			for key in data:
+				dic[key] = data[key]
+
+			print(dic)	
+			dataToSend = json.dumps(dic)
+			headers = {'Content-Type': 'application/json'}
+			req = requests.post(url,data=dataToSend, headers=headers)
+			print(req.json())
+			return render(request, 'main/stationPeer.html',{"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":False,"flagSS":False,"flagSP":True,"flagTP":False,"flagC":False})
+
 	dataToSend = json.dumps(dic)
 	headers = {'Content-Type': 'application/json'}
 	req = requests.post(url,data=dataToSend, headers=headers)
 	print(req.json())
-	return render(request, 'main/stationPeer.html',{"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":False,"flagSS":False,"flagSP":True,"flagTP":False,"flagC":False})
+	return render(request,'main/stationPeer.html')
+	
+	
 
 def postTxPower(request):
 	url = "http://httpbin.org/post"
 	#url = "http://192.168.0.25:5000/changeIP"
 	data = request.GET
-	
-	#dic = {"data":"123"}
 	dic={}
-	for key in data:
-		dic[key] = data[key]
-
+	#print(data)
+	#dic = {"data":"123"}
+	#dic = {"data":"123"}
+	print(verify_Wlan(request,data))
+	if(verify_Wlan(request,data) and not (len(data)==0)):	
 		
+		for key in data:
+			print(key)
+			dic[key] = data[key]
+
+		dataToSend = json.dumps(dic)
+		headers = {'Content-Type': 'application/json'}
+		req = requests.post(url,data=dataToSend, headers=headers)
+		print(req.json())
+		return render(request, 'main/SetTxPower.html',{"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":False,"flagSS":False,"flagSP":False,"flagTP":True,"flagC":False})
+	
 	dataToSend = json.dumps(dic)
 	headers = {'Content-Type': 'application/json'}
 	req = requests.post(url,data=dataToSend, headers=headers)
 	print(req.json())
-	return render(request, 'main/menu.html',{"flagEL":False,"flagSC":False,"flagLS":False,"flagIPC":False,"flagSS":False,"flagSP":False,"flagTP":True,"flagC":False})
+	return render(request,'main/SetTxPower.html')
 
 
 def get(request):
@@ -189,20 +255,24 @@ def get(request):
     #producer.send('numtest', value=data)
 	return render(request, 'main/menu.html')
 
-def verify_mac(mac_address):
+def verify_mac(request,mac_address):
 	if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", mac_address.lower()):
 		return 1
+	messages.warning(request, 'Please enter a valid Mac address.')
 	return 0
 
-def verify_IP(IP_address):
+def verify_IP(request,IP_address):
 	a = IP_address.split('.')
-	if len(a) != 4:
+	if len(a) is not 4:
+		messages.warning(request, 'Please insert a valid IP.')
 		return 0
 	for x in a:
 		if not x.isdigit():
+			messages.warning(request, 'Please insert a valid IP.')
 			return 0
 		i = int(x)
 		if i < 0 or i > 255:
+			messages.warning(request, 'Please insert a valid IP.')
 			return 0
 	return 1
 
@@ -215,3 +285,9 @@ def verify_wep(wep, size):
 	if re.match("[0-9a-f]*",wep.lower()) and len(wep)==(size/4):
 		return 1
 	return 0	
+
+def verify_Wlan(request,dic):
+	if "Wlan" not in dic:
+		messages.warning(request, 'Please select Wlan.')
+		return 0
+	return 1
