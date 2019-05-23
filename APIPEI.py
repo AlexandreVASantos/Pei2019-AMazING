7a6891414b430283d7f1c782f51b82b352044f9d
Fábio Nunes <fabio.rnunes@ua.pt>
2019-05-23 16:36:29 +0100

    versao toda funcional beta

--- APIPEI.py
+++ APIPEI.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify, request, json
+from flask import Flask, request, json
 from flask_restful import Api, Resource, reqparse
 import subprocess
 
-# https://wireless.wiki.kernel.org/en/users/documentation/hostapd
-
 app = Flask(__name__)
 api = Api(app)
-flag=0
+flagAP=0
 
 @app.route("/")
 def hello():
@@ -14,195 +12,238 @@ def hello():
 
 class connection(Resource):
 	def post(self):
-		args=request.get_json()		
-		if(len(args) == 3):
-			SSID = args.get('SSID')
-			PASS = args.get('PASS')
-			NetwC = args.get('NetwC')
-			command = "wpa_passphrase " + SSID + " " + PASS + " | sudo tee /etc/wpa_supplicant.conf"
-			out = subprocess.check_output(command, shell=True)
-			command2 = "sudo wpa_supplicant -B -c /etc/wpa_supplicant.conf -i " + NetwC
-			out = out + subprocess.check_output(command2, shell=True)
-			command3 = "sudo dhclient " + NetwC
-			out = out + subprocess.check_output(command3, shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({'Not the right ammount of args': 'needs to be 3 or 4'})
-		return out
+		try:
+			global flagAP
+			args=request.get_json()		
+			if(len(args) == 3):
+				SSID = args.get('SSID')
+				PASS = args.get('PASS')
+				NetwC = args.get('NetwC')
+				if(flagAP==1 and NetwC=="wlp5s0"):
+					out = json.dumps({'Message': 'AccessPoint already active'})
+					out = out.decode("utf-8")
+					return out
+				command = "wpa_passphrase " + SSID + " " + PASS + " | sudo tee /etc/wpa_supplicant.conf"
+				out = subprocess.check_output(command, shell=True)
+				command2 = "sudo wpa_supplicant -B -c /etc/wpa_supplicant.conf -i " + NetwC
+				out = out + subprocess.check_output(command2, shell=True)
+				command3 = "sudo dhclient -v " + NetwC
+				out = out + subprocess.check_output(command3, shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect arg number, needs to be 3'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
-#curl -H "Content-Type: application/json"  localhost:5000/connection -X post -d '{"NetwC":"wlp1s0","SSID":"IoT-privacy","PASS":"deadpool2"}'
-
-
-class disconnection(Resource):
+class disconnect(Resource):
 	def get(self):
-		out = subprocess.check_output("sudo killall wpa_supplicant", shell=True)
-		out = out + subprocess.check_output("sudo killall dhclient", shell=True)
-		out = out.decode("utf-8") 
-		return out
-
+		try:		
+			out = subprocess.check_output("sudo killall wpa_supplicant", shell=True)
+			out = out + subprocess.check_output("sudo killall dhclient", shell=True)
+			out = out.decode("utf-8") 
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
 class changeIP(Resource):
 	def post(self):
-		args=request.get_json()
-		IP = args.get('IP')
-		NetwC = args.get('NetwC')
-		if(len(args) == 2):
-			command = "sudo ifconfig " + NetwC + " " + IP + " netmask 255.255.255.0"
-			out = subprocess.check_output(command, shell=True)
+		try:
+			args=request.get_json()
+			IP = args.get('IP')
+			NetwC = args.get('NetwC')
+			netmask = args.get('Netmask')
+			if(len(args) == 3):
+				command = "sudo ifconfig " + NetwC + " " + IP + " netmask " + Netmask
+				out = subprocess.check_output(command, shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect arg number, needs to be 2'})
 			out = out.decode("utf-8")
-		else:
-			out = jsonify({'Not the right ammount of args': 'needs to be 2'})
-		out = subprocess.check_output("curl localhost:5000/changeIP", shell=True)
-		out = out.decode("utf-8")
-		return out
-
-		# exemplo: curl -H "Content-Type: application/json"  localhost:5000/connection -X post -d '{"IP":"192.168.0.16"}'
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
 class getifconfig(Resource):
 	def post(self):
-		args = request.get_json()
-		NetwC = args.get('NetwC')
-		if(len(args) == 1):
-			out = subprocess.check_output("sudo ifconfig | grep -m1 '" + NetwC + "' -A 1", shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({"Incorret args": 'needs to be 1'})
-		return out
-
+		try:
+			args = request.get_json()
+			NetwC = args.get('NetwC')
+			if(len(args) == 1):
+				out = subprocess.check_output("sudo ifconfig | grep -m1 '" + NetwC + "' -A 1", shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect arg number, needs to be 1'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))		
 
 class getlist(Resource):
 	def get(self):
-		out = subprocess.check_output("iw list", shell=True)
-		out = out.decode("utf-8")
-		return out
+		try:
+			out = subprocess.check_output("iw list", shell=True)
+			out = out.decode("utf-8")
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
-# exemplo: curl localhost:5000/getlist
-
-class CreateAcessPoint(Resource):
+class CreateAccessPoint(Resource):
 	def post(self):
-		args=request.get_json()
-		out = "null"
-		return out
+		try:
+			global flagAP
+			if(flagAP==1):
+				out = json.dumps({'Message': 'AccessPoint already active'})
+				out = out.decode("utf-8")
+				return out
+			args=request.get_json()
+			APSSID = args.get('APSSID')
+			APPW = args.get('APPW')
+			Channel = args.get('Channel')
+			RangeStart = args.get('RangeStart')
+			RangeEnd = args.get('RangeEnd')
+			hw_mode = args.get('hw_mode')
+			DFGateway = args.get('DFGateway')
+			Netmask = args.get('Netmask')
+			Leasetime = args.get('Leasetime')
+			out = subprocess.check_output('printf "interface=wlp5s0\ndriver=nl80211\nssid=' + APSSID + '\nhw_mode=' + hw_mode + '\nchannel='+ Channel + '\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=3\nwpa_passphrase=' + APPW + '\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\n"  > /etc/hostapd/hostapd.conf', shell=True)
+			out = subprocess.check_output('printf "interface=wlp5s0\ndhcp-range=' + RangeStart + ',' + RangeEnd + ',' + Netmask + ',' + Leasetime + 'h\ndhcp-option=3\ndhcp-option=6" > /etc/dnsmasq.conf', shell=True)
+			out = subprocess.check_output('sysctl -w net.ipv4.ip_forward=1', shell=True)
+			out = subprocess.check_output("sudo ifconfig wlp5s0 " + DFGateway + " netmask " + Netmask, shell=True)
+			out = subprocess.check_output("sudo hostapd -B /etc/hostapd/hostapd.conf", shell=True)		
+			out = subprocess.check_output("sudo /etc/init.d/dnsmasq restart", shell=True)
+			out = out.decode("utf-8")
+			flagAP=1
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
-class event(Resource):
-	def post(self):
-		args=request.get_json()
-		option = args.get('option')
-		if(len(args) == 1):
-			if(option==1):
-				out = subprocess.check_output("iw event", shell=True)
+class StopAccessPoint(Resource):
+	def get(self):
+		try:
+			global flagAP
+			if(flagAP == 1):
+				out = subprocess.check_output("sudo service dnsmasq stop", shell=True)
+				out = subprocess.check_output("sudo killall hostapd", shell=True)
 				out = out.decode("utf-8")
-			elif(option==2):
-				out = subprocess.check_output("iw event -t", shell=True)
-				out = out.decode("utf-8")
-			elif(option==3):
-				out = subprocess.check_output("iw event -f", shell=True)
-				out = out.decode("utf-8")
+				flagAP=0
+				return out
 			else:
-				out = jsonify({"Incorret option": 'needs to be 1-3'})
-		else:
-			out = jsonify({"Incorret args": 'needs to be 1'})
+				out = json.dumps({'Message':'Oops, there is no AccessPoint to stop.'})
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))		
 
-		return out
-# exemplo: curl localhost:5000/event
 
 class scan(Resource):
 	def post(self):
-		args = request.get_json()
-		NetwC = args.get('NetwC')
-		if(len(args) == 1):
-			out = subprocess.check_output("sudo iw dev'" + NetwC + "' scan", shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({'Not the right ammount of args': 'needs to receive NetworkCard'})
-		return out
-#exemplo: curl localhost:5000/scan
+		try:
+			args = request.get_json()
+			NetwC = args.get('NetwC')
+			if(len(args) == 1):
+				out = subprocess.check_output("sudo iw dev'" + NetwC + "' scan", shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect args, needs to receive the NetworkCard'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
 class localwireless(Resource):
 	def post(self):
-		args = request.get_json()
-		NetwC = args.get('NetwC')
-		if(len(args) == 1):
-			out = subprocess.check_output("sudo iw " + NetwC + " scan | grep 'SSID\|freq\|signal'", shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({'Not the right ammount of args': 'needs to receive NetworkCard'})
-		return out
-		# exemplo: curl -H "Content-Type: application/json"  localhost:5000/localwireless -X post -d '{"NetwC":"wlp1s0"}'
+		try:
+			args = request.get_json()
+			NetwC = args.get('NetwC')
+			if(len(args) == 1):
+				out = subprocess.check_output("sudo ifconfig " + NetwC + " up", shell=True)
+				out = subprocess.check_output("sudo iw " + NetwC + " scan | grep 'SSID\|freq\|signal'", shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect args, needs to receive the NetworkCard'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 	
 class link(Resource):
 	def post(self):
-		args = request.get_json()
-		NetwC = args.get('NetwC')
-		if(len(args) == 1):
-			out = subprocess.check_output("iw dev " + NetwC + " link", shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({'Not the right ammount of args': 'needs to receive NetworkCard'})
-		return out
-#exemplo: curl localhost:5000/link
+		try:
+			args = request.get_json()
+			NetwC = args.get('NetwC')
+			if(len(args) == 1):
+				out = subprocess.check_output("iw dev " + NetwC + " link", shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect args, needs to receive the NetworkCard'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
 class stationstats(Resource):
 	def post(self):
-		args = request.get_json()
-		NetwC = args.get('NetwC')
-		if(len(args) == 1):
-			out = subprocess.check_output("iw dev " + NetwC + " station dump", shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({'Not the right ammount of args': 'needs to receive NetworkCard'})
-		return out
-#exemplo: curl localhost:5000/stationstats
+		try:
+			args = request.get_json()
+			NetwC = args.get('NetwC')
+			if(len(args) == 1):
+				out = subprocess.check_output("iw dev " + NetwC + " station dump", shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect args, needs to receive the NetworkCard'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
+
 
 class stationpeerstats(Resource):
 	def get(self):
-		args=request.get_json()
-		NetwC = args.get('NetwC')
-		MAC = args.get('MAC')
-		if(len(args) == 2):
-			out = subprocess.check_output("sudo iw dev " + NetwC + " station get " + MAC, shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({'Not the right ammount of args': 'needs to receive NetworkCard'})
-		return out
+		try:
+			args=request.get_json()
+			NetwC = args.get('NetwC')
+			MAC = args.get('MAC')
+			if(len(args) == 2):
+				out = subprocess.check_output("sudo iw dev " + NetwC + " station get " + MAC, shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect args, needs to receive the NetworkCard'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 		
-# exemplo: curl -H "Content-Type: application/json"  localhost:5000/stationpeerstats -X post -d '{"MAC":"<mac-address>"}'
-
 class modtxhtmcsbitrates(Resource):
 	def post(self):
-		args = request.get_json()
-		NetwC = args.get('NetwC')
-		Lbits = args.get('Lbits')
-		if(len(args) == 1):
-			out = subprocess.check_output("iw " + NetwC + " set bitrates " + Lbits, shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({"Not the right ammount of args": 'needs to receive NetworkCard and Lbits'})
-		return out
-# exemplo: curl -H "Content-Type: application/json"  localhost:5000/modtxhtmcsbitrates -X post -d '{"mcs":"numeros"}'
+		try:
+			args = request.get_json()
+			NetwC = args.get('NetwC')
+			Lbits = args.get('Lbits')
+			if(len(args) == 1):
+				out = subprocess.check_output("iw " + NetwC + " set bitrates " + Lbits, shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect args, needs to receive the NetworkCard and the bits'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
 class settingtxpowerdev(Resource):
 	def post(self):
-		args = request.get_json()
-		NetwC = args.get('NetwC')
-		Type = args.get('Type')
-		Power = args.get('Power')
-		if(len(args) == 1):
-			out = subprocess.check_output("iw dev " + NetwC + " " + Type + " " + Power, shell=True)
-			out = out.decode("utf-8")
-		else:
-			out = jsonify({'Not the right ammount of args': 'needs to receive NetworkCard'})
-		return out
-# exemplo: curl -H "Content-Type: application/json"  localhost:5000/settingtxpowerdev -X post -d '{"mcs":"numeros"}'
-
+		try:
+			args = request.get_json()
+			NetwC = args.get('NetwC')
+			Type = args.get('Type')
+			Power = args.get('Power')
+			if(len(args) == 1):
+				out = subprocess.check_output("iw dev " + NetwC + " " + Type + " " + Power, shell=True)
+				out = out.decode("utf-8")
+			else:
+				out = json.dumps({'Message': 'Incorrect args, needs to receive the NetworkCard'})
+			return out
+		except subprocess.CalledProcessError as e:
+			return json.dumps(e.output.decode("utf-8"))
 
 api.add_resource(connection, '/connection')					#POST e GET
-api.add_resource(disconnection, '/disconnection')			#GET
+api.add_resource(disconnect, '/disconnect')					#GET
 api.add_resource(changeIP, '/changeIP')						#POST e GET
 api.add_resource(getifconfig,'/getifconfig')				#POST
 api.add_resource(getlist, '/getlist')						#GET
-api.add_resource(CreateAcessPoint, '/CreateAcessPoint')		#POST
-api.add_resource(event, '/event')							#POST
+api.add_resource(CreateAccessPoint, '/CreateAccessPoint')	#POST
+api.add_resource(StopAccessPoint, '/StopAccessPoint')		#GET
 api.add_resource(scan,'/scan')								#POST
 api.add_resource(localwireless,'/localwireless')			#POST
 api.add_resource(link,'/link')								#POST
