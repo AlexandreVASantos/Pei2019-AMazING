d4b8017db904ff04dd55d59d6c3be7271bb490da
AlexandreVASantos <43809410+AlexandreVASantos@users.noreply.github.com>
2019-06-03 15:51:38 +0100

    to be continued

--- init_node.sh
+++ init_node.sh
@@ -7,3 +7,5 @@ echo 'amazing' | sudo -S bash /home/amazing/dont_delete/wake_up.sh
 source /home/amazing/dont_delete/venv/bin/activate
 
 sudo python3 /home/amazing/dont_delete/APIPEI.py &
+
+sudo python3 /home/amazing/dont_delete/node_init.py &
